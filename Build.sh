0f38817339a5891f017b0500627f29b5228e5b2b
CharinduThisara <thisaraimc@gmail.com>
2024-03-18 09:23:08 +0530

    Remove unnecessary files and update .gitignore

--- Build.sh
+++ Build.sh
@@ -1,9 +1,72 @@
+#!/bin/bash
+
+# Function to perform the liveness probe
+perform_liveness_probe() {
+    curl -s -o /dev/null -w "%{http_code}" "$url"
+}
+
 echo 3 | sudo update-alternatives --config java
 JAVA_HOME=`/usr/lib/jvm/java-1.8.0-openjdk-amd64` mvn clean install
 
-rm -rf ../wso2is-7.0.0/repository/components/dropins/org.wso2.custom.user.operation.event.listener-1.0-SNAPSHOT.jar
+rm -rf ../wso2is-7.0.0
+unzip ../wso2is-7.0.0 -d ../
 cp target/org.wso2.custom.user.operation.event.listener-1.0-SNAPSHOT.jar ../wso2is-7.0.0/repository/components/dropins
+cp lib ../wso2is-7.0.0/repository/components/dropins
+rm -rf ../wso2is-7.0.0/repository/conf/deployment.toml
+cp deployment.toml ../wso2is-7.0.0/repository/conf
 
 echo 2 | sudo update-alternatives --config java
 
 ../wso2is-7.0.0/bin/wso2server.sh 
+
+# Define the URL for liveness probe
+url="https://localhost:9443/carbon/admin/login.jsp"
+
+while true; do
+    response_code=$(perform_liveness_probe)
+    if [ "$response_code" -eq 200 ]; then
+        # Server is live, break the loop
+        break
+    else
+        # Server is not live, wait for a while and try again
+        sleep 5  # Adjust the sleep interval as needed
+    fi
+done
+
+# Store the curl response in a variable
+response=$(curl -X 'POST' \
+'https://localhost:9443/scim2/Users' \
+-H 'accept: application/scim+json' \
+-H 'Content-Type: application/scim+json' \
+-d '{
+"schemas": [],
+"name": {
+  "givenName": "Kim",
+  "familyName": "Berry"
+},
+"userName": "kim",
+"password": "MyPa33w@rd",
+"emails": [
+  {
+    "type": "home",
+    "value": "kim@gmail.com",
+    "primary": true
+  },
+  {
+    "type": "work",
+    "value": "kim@wso2.com"
+  }
+],
+"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
+  "employeeNumber": "1234A",
+  "manager": {
+    "value": "Taylor"
+  }
+}
+}')
+
+# Extract the "id" using jq
+user_id=$(echo "$response" | jq -r '.id')
+
+# Print the user ID
+echo "User ID: $user_id"
--- src/main/java/org/wso2/custom/user/operation/event/listener/CustomUserOperationEventListener.java
+++ src/main/java/org/wso2/custom/user/operation/event/listener/CustomUserOperationEventListener.java
@@ -1,19 +1,15 @@
 package org.wso2.custom.user.operation.event.listener;
 
-
-import org.wso2.carbon.user.api.Permission;
 import org.wso2.carbon.user.core.UserStoreException;
 import org.wso2.carbon.user.core.UserStoreManager;
 import org.wso2.carbon.user.core.common.*;
-import org.wso2.carbon.user.core.model.Condition;
-import org.wso2.carbon.user.core.model.UniqueIDUserClaimSearchEntry;
-import org.wso2.carbon.user.core.model.UserClaimSearchEntry;
-
+    
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.oss.driver.api.core.cql.PreparedStatement;
-import com.datastax.oss.driver.api.core.uuid.Uuids;
 
-import java.util.List;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+
+import java.net.InetSocketAddress;
+
 import java.util.Map;
 
 
@@ -22,17 +18,22 @@ import java.util.Map;
  */
 public class CustomUserOperationEventListener extends AbstractUserOperationEventListener {
 
+    private CqlSession session;
+
     private String systemUserPrefix = "system_";
 
     private static final String NODE_IP = "127.0.0.1";
     private static final int PORT = 9042;
     private static final String KEYSPACE = "my_keyspace";
+    private static final String LOCAL_DATACENTER = "datacenter1";
+
 
     private static final String INSERT_USER_QUERY = "INSERT INTO users (user_id, username, credential, role_list, claims, profile) VALUES (?, ?, ?, ?, ?, ?)";
 
-    private static final String INIT_SCHEMA_QUERY = "CREATE KEYSPACE IF NOT EXISTS my_keyspace "
-            + "WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-            + "CREATE TABLE IF NOT EXISTS my_keyspace.users ("
+    String createKeyspaceQuery = "CREATE KEYSPACE IF NOT EXISTS my_keyspace "
+            + "WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};";
+
+    String createTableQuery = "CREATE TABLE IF NOT EXISTS my_keyspace.users ("
             + "user_id UUID PRIMARY KEY, "
             + "username TEXT, "
             + "credential BLOB, "
@@ -40,10 +41,36 @@ public class CustomUserOperationEventListener extends AbstractUserOperationEvent
             + "claims MAP<TEXT, TEXT>, "
             + "profile TEXT)";
 
-
     public CustomUserOperationEventListener() {
         super();
+
+        // initializeCassandra();
     }
+
+    public void initializeCassandra(){
+        
+        connect(NODE_IP, PORT, LOCAL_DATACENTER);
+        System.out.println("Connected to Cassandra");
+
+        session.execute(createKeyspaceQuery);
+        System.out.println("Keyspace created");
+      
+        session.execute(createTableQuery);
+        System.out.println("Table created");
+        
+    }
+
+    public void connect(String node, Integer port, String dataCenter) {
+        CqlSessionBuilder builder = CqlSession.builder();
+        builder.addContactPoint(new InetSocketAddress(node, port));
+        builder.withLocalDatacenter(dataCenter);
+
+        this.session = builder.build();
+
+        System.out.println("Connected to Cassandra");
+    }
+
+
 
     @Override
     public boolean doPostAddUser(String userName, Object credential, String[] roleList, Map<String, String> claims, String profile, UserStoreManager userStoreManager) throws UserStoreException {
@@ -57,6 +84,7 @@ public class CustomUserOperationEventListener extends AbstractUserOperationEvent
         printArray(roleList);
         System.out.printf("User Profile: %s\n", profile);
         
+        connect(NODE_IP, PORT,LOCAL_DATACENTER);
         // try (CqlSession session = CqlSession.builder()
         // .addContactPoint(new InetSocketAddress(NODE_IP, PORT))
         // .withKeyspace(KEYSPACE)
@@ -86,7 +114,6 @@ public class CustomUserOperationEventListener extends AbstractUserOperationEvent
         //     return false;
         // }
 
-     
         return true;
     }
     
@@ -115,7 +142,29 @@ public class CustomUserOperationEventListener extends AbstractUserOperationEvent
                 System.out.println("User deleted successfully");
                 System.out.println("User Name: " + s);
                 System.out.println("User Store Manager: " + userStoreManager);
+                // connect(NODE_IP, PORT,LOCAL_DATACENTER);
 
+                System.out.println("Connecting to Cassandra...");
+                // Establishing connection to Cassandra
+                // try (CqlSession session = new CqlSessionBuilder()
+                //         .addContactPoint(new InetSocketAddress(NODE_IP, PORT))
+                //         .withLocalDatacenter("datacenter1") // Adjust to your local datacenter name
+                //         .build()) {
+                //     System.out.println("Connected to Cassandra.");
+                //     // Writing data to the user_data table
+                   
+                // } catch (Exception e) {
+                //     System.err.println("Error: " + e);
+                // }
+
+                this.session = new CqlSessionBuilder()
+                        .addContactPoint(new InetSocketAddress(NODE_IP, PORT))
+                        .withLocalDatacenter("datacenter1") // Adjust to your local datacenter name
+                        .build();
+                System.out.println("Connected to Cassandra.");
+                    
+    
+                
         return true;
     }
 
